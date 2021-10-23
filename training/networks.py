4bca9582e805209c71e8991cefb559c83320a282
hama2386 <jwchlrkdgus@yonsei.ac.kr>
2021-10-23 17:35:39 +0000

    new era begin... or not

--- training/networks.py
+++ training/networks.py
@@ -292,7 +292,7 @@ class SynthesisLayer(torch.nn.Module):
         if self.resolution < 256:
 
             num_patches = self.resolution**2
-            dim = self.in_channels
+            dim = in_channels
             dim_ff = dim*4
             self.to_embed = nn.Sequential(
                 Rearrange('b c h w -> b (h w) c'),
